0d7e75e8506a21869ddcf673f0807ea2c2873fe4
Makeademic <kaplanwizard@gmail.com>
2025-05-14 13:39:59 +0000

    repasted eleventy config

--- eleventy.config.js
+++ eleventy.config.js
@@ -23,8 +23,6 @@ export default async function(eleventyConfig) {
 		})
 		.addPassthroughCopy("./content/feed/pretty-atom-feed.xsl");
 
-	
-
 	// Run Eleventy when these files change:
 	// https://www.11ty.dev/docs/watch-serve/#add-your-own-watch-targets
 
