13111c010859039adf5def78b06dfc22c5481186
Stewart Varner <stewart.varner@gmail.com>
2025-05-16 13:34:22 +0000

    I changed the name and installed dependencies

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,10 @@
 export default {
-	title: "Dream Lab Blog",
+	title: "Stewart's Dream Lab Blog",
 	url: "https://makeademic.github.io/dream-lab-blog/",
 	language: "en",
 	description: "I am writing about my experiences at Dream Lab 2025",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Stewart Varner",
+		email: "svarner@upenn.edu",
 	}
 }
